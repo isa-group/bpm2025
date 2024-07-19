17aac4d92ed082f5d58292975eb13e7db9f7281b
Fernando Fernández <ferferga@hotmail.com>
2024-07-19 11:36:03 +0200

    chore: cleanup folder tree of astrowind template
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -3,57 +3,91 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import typescriptParser from '@typescript-eslint/parser';
+import stylistic from '@stylistic/eslint-plugin';
+import fileProgress from 'eslint-plugin-file-progress';
+import gitignore from 'eslint-config-flat-gitignore';
+
+const CI_environment = process.env.CI ? 0 : 1;
 
 export default [
   js.configs.recommended,
   ...eslintPluginAstro.configs['flat/recommended'],
   ...tseslint.configs.recommended,
+  tseslint.configs.eslintRecommended,
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node,
+        ...globals.node
       },
-    },
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: false
+      }
+    }
+  },
+  {
+    ...stylistic.configs.customize({
+      quotes: 'single',
+      semi: true,
+      commaDangle: 'never',
+      braceStyle: '1tbs',
+      arrowParens: false,
+      blockSpacing: true
+    }),
+    name: '(@stylistic) Extended config from plugin'
   },
   {
     files: ['**/*.astro'],
     languageOptions: {
       parser: astroEslintParser,
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-      },
-    },
-  },
-  {
-    files: ['**/*.{js,jsx,astro}'],
-    rules: {
-      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-    },
+        parser: tseslint.parser,
+        extraFileExtensions: ['.astro']
+      }
+    }
   },
   {
     // Define the configuration for `<script>` tag.
     // Script in `<script>` is assigned a virtual file name with the `.js` extension.
     files: ['**/*.{ts,tsx}', '**/*.astro/*.js'],
     languageOptions: {
-      parser: typescriptParser,
+      parser: tseslint.parser
     },
     rules: {
-      // Note: you must disable the base rule as it can report incorrect errors
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/no-non-null-assertion': 'off',
-    },
+      '@typescript-eslint/no-non-null-assertion': 'off'
+    }
   },
   {
-    ignores: ['dist', 'node_modules', '.github', 'types.generated.d.ts', '.astro'],
+    rules: {
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/jsx-one-expression-per-line': 'off'
+    }
   },
+  {
+    ...stylistic.configs['disable-legacy'],
+    name: 'Environment config - Disable legacy rules'
+  },
+  {
+    ignores: [
+      '**/.git',
+      ...gitignore().ignores
+    ]
+  },
+  /** File progress plugin */
+  {
+    name: '(eslint) Linting progress report',
+    settings: {
+      progress: {
+        successMessage: 'Linting done!'
+      }
+    },
+    plugins: {
+      'file-progress': fileProgress
+    },
+    rules: {
+      'file-progress/activate': CI_environment
+    }
+  }
 ];
