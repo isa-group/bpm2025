afcec911efa5b4724805c96e2bfcc17503e9aec8
Fernando Fernández <ferferga@hotmail.com>
2024-10-01 17:48:20 +0000

    feat: better keywords, robots as a file as well

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from 'astro';
+import { metadata } from '@/config.json';
+
+const getRobotsTxt = (sitemapURL: URL) => `
+User-agent: *
+${metadata.robots.index ? 'Allow' : 'Disallow'}: /
+
+Sitemap: ${sitemapURL.href}
+`.trimStart();
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL('sitemap-index.xml', site);
+  return new Response(getRobotsTxt(sitemapURL));
+};
