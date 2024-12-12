6df14def2e384322a9c04bf43334a08b91346451
Fernando Fernández <ferferga@hotmail.com>
2024-12-12 17:24:10 +0100

    refactor: make mailing more configurable as well
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- packages/backend/src/routes/payment.ts
+++ packages/backend/src/routes/payment.ts
@@ -1,39 +1,6 @@
 import { defineEventHandler, getQuery, readBody } from 'h3';
-import { db } from '../util/db.ts';
 import { router } from '../app.ts';
-import { generateOrderInvoice } from '../util/invoicing';
-import { sendConfirmationEmail } from '../util/mailing';
 import { isEmpty } from '@bpm2025-website/shared/validation';
-
-/**
- * Runs all the logic after the payment has been confirmed
- */
-async function postPaymentConfirm(order_id: string) {
-  const full_order = await db.order.findUniqueOrThrow({
-    where: {
-      id: order_id
-    },
-    include: {
-      user: true,
-      product: true,
-      discount_order: {
-        include: {
-          discount: true
-        }
-      }
-    }
-  });
-
-  const invoice_path = await generateOrderInvoice({
-    order: full_order,
-    vat_rate: 0.21
-  });
-  await sendConfirmationEmail({
-    order_id,
-    destination_email: full_order.user.email,
-    invoice_path
-  });
-}
 
 /**
  * Gets the payment result from Redsys TPV. Is valid for both
--- packages/backend/src/util/mailing/worker.ts
+++ packages/backend/src/util/mailing/worker.ts
@@ -11,14 +11,21 @@ export interface WorkerData {
   MAIL_FROM: string;
   MAIL_REPLY_TO?: string;
   MAIL_SUBJECT: string;
+  MAIL_CONTENT?: string;
 }
 
 declare const workerData: WorkerData;
 
 export interface Inputs {
   order_id: string;
-  destination_email: string;
-  invoice_path: string;
+  mail: {
+    destination: string;
+    content?: string;
+    subject?: string;
+    from?: string;
+    reply_to?: string;
+  };
+  invoice_path?: string;
 }
 
 const transporter = createTransport({
@@ -32,20 +39,30 @@ const transporter = createTransport({
   pool: true
 });
 
+transporter.verify((err) => {
+  if (err) {
+    console.error('SMTP connection could not be verified. Please check your configuration\n\n', err);
+  }
+});
+
 parentPort?.on('message', async (inputs: Inputs) => {
   try {
     await transporter.sendMail({
-      from: workerData.MAIL_FROM,
-      to: inputs.destination_email,
-      subject: workerData.MAIL_SUBJECT.replace('{order_id}', inputs.order_id),
-      replyTo: workerData.MAIL_REPLY_TO,
-      text: 'Please find your invoice attached.',
-      attachments: [
-        {
-          filename: 'invoice.pdf',
-          path: inputs.invoice_path
-        }
-      ]
+      from: inputs.mail.from ?? workerData.MAIL_FROM,
+      to: inputs.mail.destination,
+      subject: (workerData.MAIL_SUBJECT ?? inputs.mail.subject).replace('{order_id}', inputs.order_id),
+      replyTo: inputs.mail.reply_to ?? workerData.MAIL_REPLY_TO,
+      text: inputs.mail.content ?? workerData.MAIL_CONTENT ?? 'Please find your invoice attached.',
+      ...(inputs.invoice_path
+        ? {
+            attachments: [
+              {
+                filename: 'invoice.pdf',
+                path: inputs.invoice_path
+              }
+            ]
+          }
+        : {})
     });
     parentPort?.postMessage({
       success: true,
