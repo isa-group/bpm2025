b36f7cf7bc384ff8633f616a7cccb35e11b6e9dd
Fernando Fernández <ferferga@hotmail.com>
2024-12-20 13:06:52 +0100

    refactor: reimplement credit card payments
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- packages/shared/validation/data.ts
+++ packages/shared/validation/data.ts
@@ -1,6 +1,9 @@
-import type { OrderPayload, SignupPayload } from '..';
+import type { OrderPayload, RedsysResponse, SignupPayload } from '..';
 import { isObj, isStr, isNumber } from '.';
 
+/**
+ * Validates the user body for creating a new user in the backend
+ */
 export function validateUserBody(
   payload: unknown
 ): payload is SignupPayload {
@@ -21,8 +24,27 @@ export function validateUserBody(
   return correctName && correctEmail && correctBillingAddress;
 }
 
+/**
+ * Validates the order body for creating a new order in the backend
+ */
 export function validateOrderBody(
   payload: unknown
 ): payload is OrderPayload {
   return validateUserBody(payload) && 'product_id' in payload && isNumber(payload.product_id);
+}
+
+/***
+ * Ensures the response from Redsys TPV follows our expected format
+ */
+export function validateRedsysResponse(
+  payload: unknown
+): payload is RedsysResponse {
+  return isObj(payload)
+    && 'Ds_SignatureVersion' in payload
+    && 'Ds_Signature' in payload
+    && 'Ds_MerchantParameters' in payload
+    && isStr(payload.Ds_SignatureVersion)
+    && payload.Ds_SignatureVersion === 'HMAC_SHA256_V1'
+    && isStr(payload.Ds_Signature)
+    && isStr(payload.Ds_MerchantParameters);
 }
