c725ed7678148138c55da356636ed795f5cf74c3
Fernando Fernández <ferferga@hotmail.com>
2025-04-04 12:00:43 +0200

    feat(keynotes): add Josep Carmona
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- packages/frontend/src/data/keynotes.ts
+++ packages/frontend/src/data/keynotes.ts
@@ -1,4 +1,5 @@
 import ShaziaSadiq from '#/assets/images/keynotes/ShaziaSadiq.jpg';
+import JosepCarmona from '#/assets/images/keynotes/JosepCarmona.jpg';
 
 export const keynotes = [
   {
@@ -17,6 +18,24 @@ export const keynotes = [
       The Australian Research Council College of Experts 2018-2021.</p>
       <br />
       <p>During her keynote, Shazia will talk about “The Essential Toolkit for Process Scientists: Comprehension, Compliance and Change.”</p>
+    `
+  },
+  {
+    name: 'Josep Carmona',
+    subtitle: 'Universitat Politècnica de Catalunya, Spain',
+    imageClass: 'aspect-ratio-[16/14] object-[0_90%] lg:aspect-ratio-[1/1]',
+    image: JosepCarmona,
+    description: `
+      <p><b>Josep Carmona</b> is a full professor of computer science at the Universitat Politècnica de Catalunya (UPC) and
+      CEO of Process Talks, a company founded to empower people when they sit in front of a computer. As a researcher, he enjoys solving
+      complex problems by connecting complementary disciplines—a habit that has led him to work in areas such as concurrent systems,
+      VLSI, BPM, process mining, data science, natural language processing, and generative AI. Josep co-authored the book
+      Conformance Checking (Springer, 2018) and co-edited the Process Mining Handbook (Springer, 2022), the latter of which is one of
+      the most downloaded books in the field of process mining today.</p>
+      <br />
+      <p>As an entrepreneur, he is building a technology company amidst the unprecedented revolution brought on by the disruption of
+      generative AI. This exciting journey is filled with lessons learned and best practices that Josep plans to share
+      with the BPM community in this keynote.</p>
     `
   }
 ];
