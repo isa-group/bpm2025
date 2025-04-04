7afb879f781be284f07b907115a4a2ac9fb88bd4
Fernando Fernández <ferferga@hotmail.com>
2025-04-04 10:41:21 +0200

    refactor: extract keynote generation to an object
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

---
+++ packages/frontend/src/data/keynotes.ts
@@ -0,0 +1,23 @@
+import ShaziaSadiq from '#/assets/images/keynotes/ShaziaSadiq.jpg';
+
+export const keynotes = [
+  {
+    name: 'Shazia Sadiq',
+    subtitle: 'University of Queensland, Australia',
+    image: ShaziaSadiq,
+    description: `
+      <b>Shazia Sadiq</b> is a globally recognized leader in data and process management,
+      with a 25-year career as a researcher and educator focused on dismantling socio-technical barriers to technology-driven
+      transformation. Her work has significantly advanced the fields of data quality management, scalable data curation
+      process modelling and compliance, and information resilience. She has published over 200 peer-reviewed publications
+      and attracted research funding from the Australian Research Council, industry and various national and international funding
+      bodies. Shazia is an elected Fellow of the Australian Academy of Technological Sciences and Engineering, Director for the
+      ARC Industry Transformation Training Centre on Information Resilience 2021-2026, past chair of the National Committee on
+      Information and Communication Sciences at the Australian Academy of Science 2019-2022, and member of
+      The Australian Research Council College of Experts 2018-2021.
+      <br />
+      <br />
+      <p>During her keynote, Shazia will talk about “The Essential Toolkit for Process Scientists: Comprehension, Compliance and Change.”</p>
+    `
+  }
+];
