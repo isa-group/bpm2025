1faf0ad28c21d6344f144b7d3a17a2f30b3b1a18
Fernando Fernández <ferferga@hotmail.com>
2025-06-23 15:31:44 +0000

    refactor(schedule): update some hours

--- packages/frontend/src/data/schedule.ts
+++ packages/frontend/src/data/schedule.ts
@@ -14,21 +14,18 @@ export const events: ScheduleEvent[] = [
     title: 'Doctoral Consortium',
     start: new Date(2025, 7, 31, 9, 0),
     end: new Date(2025, 7, 31, 12, 0),
-    location: 'Hall 2',
     category: 'workshop'
   },
   {
     title: 'Lunch',
-    start: new Date(2025, 7, 31, 12, 0),
-    end: new Date(2025, 7, 31, 13, 30),
-    location: 'University Rectorate',
+    start: new Date(2025, 7, 31, 14, 0),
+    end: new Date(2025, 7, 31, 15, 30),
     category: 'lunch'
   },
   {
     title: 'Doctoral Consortium',
-    start: new Date(2025, 7, 31, 13, 30),
-    end: new Date(2025, 7, 31, 17, 0),
-    location: 'Hall 2',
+    start: new Date(2025, 7, 31, 16, 0),
+    end: new Date(2025, 7, 31, 17, 30),
     category: 'workshop'
   },
 
@@ -37,28 +34,25 @@ export const events: ScheduleEvent[] = [
     title: 'Workshops',
     start: new Date(2025, 8, 1, 9, 0),
     end: new Date(2025, 8, 1, 12, 30),
-    location: 'Hall 4',
     category: 'workshop'
   },
   {
     title: 'Lunch',
-    start: new Date(2025, 8, 1, 12, 30),
-    end: new Date(2025, 8, 1, 14, 0),
-    location: 'Melia\'s Restaurant',
+    start: new Date(2025, 8, 1, 14, 0),
+    end: new Date(2025, 8, 1, 15, 30),
     category: 'lunch'
   },
   {
     title: 'Workshops',
-    start: new Date(2025, 8, 1, 14, 0),
+    start: new Date(2025, 8, 1, 16, 0),
     end: new Date(2025, 8, 1, 17, 30),
-    location: 'Hall 4',
     category: 'workshop'
   },
   {
     title: 'Reception dinner',
     start: new Date(2025, 8, 1, 19, 0),
     end: new Date(2025, 8, 1, 21, 0),
-    location: 'University Rectorate',
+    location: 'Abades Triana',
     category: 'social'
   },
 
@@ -67,49 +61,55 @@ export const events: ScheduleEvent[] = [
     title: 'Keynote',
     start: new Date(2025, 8, 2, 9, 0),
     end: new Date(2025, 8, 2, 10, 0),
-    location: 'Hall 3',
     category: 'keynote'
   },
   {
     title: 'Conference',
     start: new Date(2025, 8, 2, 10, 30),
-    end: new Date(2025, 8, 2, 12, 0),
-    location: 'Hall 3',
+    end: new Date(2025, 8, 2, 12, 30),
     category: 'conference_1'
   },
   {
-    title: 'Education Forum',
+    title: 'Educators Forum',
     start: new Date(2025, 8, 2, 10, 30),
-    end: new Date(2025, 8, 2, 12, 0),
-    location: 'Hall 4',
+    end: new Date(2025, 8, 2, 12, 30),
     category: 'forum_1'
   },
   {
+    title: 'Process Technology Forum',
+    start: new Date(2025, 8, 2, 10, 30),
+    end: new Date(2025, 8, 2, 12, 30),
+    category: 'forum_2'
+  },
+  {
     title: 'Lunch',
-    start: new Date(2025, 8, 2, 12, 0),
-    end: new Date(2025, 8, 2, 13, 30),
-    location: 'Melia\'s Restaurant',
+    start: new Date(2025, 8, 2, 14, 0),
+    end: new Date(2025, 8, 2, 15, 30),
     category: 'lunch'
   },
   {
     title: 'Conference',
-    start: new Date(2025, 8, 2, 13, 30),
-    end: new Date(2025, 8, 2, 17, 0),
-    location: 'Hall 3',
+    start: new Date(2025, 8, 2, 16, 0),
+    end: new Date(2025, 8, 2, 17, 30),
     category: 'conference_1'
   },
   {
-    title: 'Education Forum',
-    start: new Date(2025, 8, 2, 13, 30),
-    end: new Date(2025, 8, 2, 17, 0),
-    location: 'Hall 4',
+    title: 'Educators Forum',
+    start: new Date(2025, 8, 2, 16, 0),
+    end: new Date(2025, 8, 2, 17, 30),
     category: 'forum_1'
+  },
+  {
+    title: 'Process Technology Forum',
+    start: new Date(2025, 8, 2, 16, 0),
+    end: new Date(2025, 8, 2, 17, 30),
+    category: 'forum_2'
   },
   {
     title: 'Guided tour',
     start: new Date(2025, 8, 2, 19, 0),
     end: new Date(2025, 8, 2, 21, 30),
-    location: 'Cathedral of Seville',
+    location: 'Reales Alcázares',
     category: 'social'
   },
   // 3 de Septiembre
@@ -117,56 +117,48 @@ export const events: ScheduleEvent[] = [
     title: 'Keynote',
     start: new Date(2025, 8, 3, 9, 0),
     end: new Date(2025, 8, 3, 10, 0),
-    location: 'Main hall',
     category: 'keynote'
   },
   {
     title: 'Conference',
     start: new Date(2025, 8, 3, 10, 30),
-    end: new Date(2025, 8, 3, 12, 0),
-    location: 'Hall 4',
+    end: new Date(2025, 8, 3, 12, 30),
     category: 'conference_1'
   },
   {
-    title: 'Industry Forum',
+    title: 'Industry & Society Forum',
     start: new Date(2025, 8, 3, 10, 30),
-    end: new Date(2025, 8, 3, 12, 0),
-    location: 'Hall 3',
+    end: new Date(2025, 8, 3, 12, 30),
     category: 'forum_2'
   },
   {
     title: 'Lunch',
-    start: new Date(2025, 8, 3, 12, 0),
-    end: new Date(2025, 8, 3, 13, 30),
-    location: 'University Rectorate',
+    start: new Date(2025, 8, 3, 14, 0),
+    end: new Date(2025, 8, 3, 15, 30),
     category: 'lunch'
   },
   {
     title: 'Conference',
-    start: new Date(2025, 8, 3, 13, 30),
-    end: new Date(2025, 8, 3, 17, 0),
-    location: 'Hall 4',
+    start: new Date(2025, 8, 3, 16, 0),
+    end: new Date(2025, 8, 3, 17, 30),
     category: 'conference_1'
   },
   {
-    title: 'Industry Forum',
-    start: new Date(2025, 8, 3, 13, 30),
-    end: new Date(2025, 8, 3, 17, 0),
-    location: 'Hall 3',
+    title: 'Industry & Society Forum',
+    start: new Date(2025, 8, 3, 16, 0),
+    end: new Date(2025, 8, 3, 17, 30),
     category: 'forum_2'
   },
   {
     title: 'Demos',
-    start: new Date(2025, 8, 3, 17, 30),
-    end: new Date(2025, 8, 3, 19, 0),
-    location: 'Hall 4',
+    start: new Date(2025, 8, 3, 16, 0),
+    end: new Date(2025, 8, 3, 17, 30),
     category: 'demos'
   },
   {
     title: 'Conference dinner',
     start: new Date(2025, 8, 3, 19, 30),
     end: new Date(2025, 8, 3, 21, 0),
-    location: 'University Rectorate',
     category: 'social'
   },
 
@@ -175,56 +167,48 @@ export const events: ScheduleEvent[] = [
     title: 'Keynote',
     start: new Date(2025, 8, 4, 9, 0),
     end: new Date(2025, 8, 4, 10, 0),
-    location: 'Hall 1',
     category: 'keynote'
   },
   {
     title: 'Conference',
     start: new Date(2025, 8, 4, 10, 30),
-    end: new Date(2025, 8, 4, 12, 0),
-    location: 'Hall 1',
+    end: new Date(2025, 8, 4, 12, 30),
     category: 'conference_1'
   },
   {
     title: 'Responsible BPM Forum',
     start: new Date(2025, 8, 4, 10, 30),
-    end: new Date(2025, 8, 4, 12, 0),
-    location: 'Hall 2',
+    end: new Date(2025, 8, 4, 12, 30),
     category: 'forum_3'
   },
   {
     title: 'Lunch',
-    start: new Date(2025, 8, 4, 12, 0),
-    end: new Date(2025, 8, 4, 13, 30),
-    location: 'University Rectorate',
+    start: new Date(2025, 8, 4, 14, 0),
+    end: new Date(2025, 8, 4, 15, 30),
     category: 'lunch'
   },
   {
     title: 'Conference',
-    start: new Date(2025, 8, 4, 13, 30),
-    end: new Date(2025, 8, 4, 17, 0),
-    location: 'Hall 1',
+    start: new Date(2025, 8, 4, 16, 0),
+    end: new Date(2025, 8, 4, 17, 30),
     category: 'conference_1'
   },
   {
     title: 'Responsible BPM Forum',
-    start: new Date(2025, 8, 4, 13, 30),
-    end: new Date(2025, 8, 4, 17, 0),
-    location: 'Hall 2',
+    start: new Date(2025, 8, 4, 16, 0),
+    end: new Date(2025, 8, 4, 17, 30),
     category: 'forum_3'
   },
   {
-    title: 'Demos',
-    start: new Date(2025, 8, 4, 17, 30),
-    end: new Date(2025, 8, 4, 19, 0),
-    location: 'Hall 1',
+    title: 'Panel',
+    start: new Date(2025, 8, 4, 16, 0),
+    end: new Date(2025, 8, 4, 17, 30),
     category: 'demos'
   },
   {
     title: 'Farewell',
     start: new Date(2025, 8, 4, 19, 30),
     end: new Date(2025, 8, 4, 21, 0),
-    location: 'ETSII',
     category: 'social'
   },
 
@@ -233,14 +217,12 @@ export const events: ScheduleEvent[] = [
     title: 'BPM in Public Administration (Spanish)',
     start: new Date(2025, 8, 5, 9, 0),
     end: new Date(2025, 8, 5, 12, 0),
-    location: 'Hall 5',
     category: 'conference_2'
   },
   {
     title: 'Lunch',
-    start: new Date(2025, 8, 5, 12, 0),
-    end: new Date(2025, 8, 5, 13, 30),
-    location: 'University Rectorate',
+    start: new Date(2025, 8, 5, 14, 0),
+    end: new Date(2025, 8, 5, 15, 30),
     category: 'lunch'
   }
 ];
