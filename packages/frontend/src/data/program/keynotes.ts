4e066c9097d039c50d6ff15664db93d1758639af
Fernando Fernández <ferferga@hotmail.com>
2025-06-24 10:24:17 +0000

    refactor: extract keynotes layout to custom component

---
+++ packages/frontend/src/data/program/keynotes.ts
@@ -0,0 +1,64 @@
+import ShaziaSadiq from '#/assets/images/program/keynotes/ShaziaSadiq.jpg';
+import JosepCarmona from '#/assets/images/program/keynotes/JosepCarmona.jpg';
+import PernilleBjorn from '#/assets/images/program/keynotes/PernilleBjorn.jpg';
+
+export const keynotes = [
+  {
+    name: 'Shazia Sadiq',
+    subtitle: 'University of Queensland, Australia',
+    image: ShaziaSadiq,
+    description: `
+      <p><b>Shazia Sadiq</b> is a globally recognized leader in data and process management,
+      with a 25-year career as a researcher and educator focused on dismantling socio-technical barriers to technology-driven
+      transformation. Her work has significantly advanced the fields of data quality management, scalable data curation
+      process modelling and compliance, and information resilience. She has published over 200 peer-reviewed publications
+      and attracted research funding from the Australian Research Council, industry and various national and international funding
+      bodies. Shazia is an elected Fellow of the Australian Academy of Technological Sciences and Engineering, Director for the
+      ARC Industry Transformation Training Centre on Information Resilience 2021-2026, past chair of the National Committee on
+      Information and Communication Sciences at the Australian Academy of Science 2019-2022, and member of
+      The Australian Research Council College of Experts 2018-2021.</p>
+      <br />
+      <p>During her keynote, Shazia will talk about “The Essential Toolkit for Process Scientists: Comprehension, Compliance and Change.”</p>
+    `
+  },
+  {
+    name: 'Josep Carmona',
+    subtitle: 'Universitat Politècnica de Catalunya, Spain',
+    imageClass: 'aspect-ratio-[16/14] object-[0_90%] lg:aspect-ratio-[1/1]',
+    image: JosepCarmona,
+    description: `
+      <p><b>Josep Carmona</b> is a full professor of computer science at the Universitat Politècnica de Catalunya (UPC) and
+      CEO of Process Talks, a company founded to empower people when they sit in front of a computer. As a researcher, he enjoys solving
+      complex problems by connecting complementary disciplines—a habit that has led him to work in areas such as concurrent systems,
+      VLSI, BPM, process mining, data science, natural language processing, and generative AI. Josep co-authored the book
+      Conformance Checking (Springer, 2018) and co-edited the Process Mining Handbook (Springer, 2022), the latter of which is one of
+      the most downloaded books in the field of process mining today.</p>
+      <br />
+      <p>As an entrepreneur, he is building a technology company amidst the unprecedented revolution brought on by the disruption of
+      generative AI. This exciting journey is filled with lessons learned and best practices that Josep plans to share
+      with the BPM community in this keynote.</p>
+    `
+  },
+  {
+    name: 'Pernille Bjørn',
+    subtitle: 'University of Copenhagen, Denmark',
+    image: PernilleBjorn,
+    imageClass: 'aspect-ratio-[16/14] object-top lg:aspect-ratio-[1/1]',
+    description: `
+      <p><b>Pernille Bjørn</b> is an ACM Distinguish member and professor at the Department of Computer Science,
+      University of Copenhagen, Denmark (DIKU). Professor Bjørn’s research is centred within the area of
+      Computer Supported Cooperative Work (CSCW), studying cooperative practices to design cooperative technologies.
+      Professor Bjørn is best known for research on distributed work (e.g., global software development),
+      large information systems (e.g., healthcare systems), and tech entrepreneurship (e.g., Palestine).
+      Currently, Professor Bjørn research interest focuses on Hybrid Work, Cooperative Virtual Reality, and Diversity,
+      Equity, and Inclusion bridging gender and neurodiversity. Professor Bjørn spent several years in North America at the
+      University of California, Irvine, Simon Fraser University, Vancouver, and lastly, as a Fulbright scholar at the
+      University of Washington. Professor Bjørn was a visiting researcher at the Indian Institute of Management, Bangalore
+      (IIM-B) in periods between 2012-2014, and published her second book: Diversity in Computer Science in 2023.</p>
+      <br />
+      <p>In this keynote, Professor Bjørn will explore the connections between research on Computer Supported Cooperative Work
+      and Business Process Management and explore the benefit of engaging in collaboration producing new forms of
+      research together and across the fields.</p>
+    `
+  }
+];
