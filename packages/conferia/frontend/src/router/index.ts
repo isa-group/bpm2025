8f35bfbe74081e5e1472872756eb6b920fec768e
Fernando Fernández <ferferga@hotmail.com>
2025-07-18 10:24:36 +0000

    feat(conferia): incorporate frontend
    Sourced from https://github.com/Conferia/bpm25 at 18/07/2025 12:11 UTC+2
    
    Files have been copied verbatim from that repository, except the following ones:
    * .github/workflows
    * public/favicon*
    * public/manifest_icons/*
    * src/assets/images
    
    These files were removed because:
    - We already have our own workflow files and we are 100% sure from the beginning
    that we will never reuse them
    - The icon files increase the size of the repository and we already have the icons from the "frontend"
    package available, so no need to duplicate them and create further cruft in our git history.
    
    Co-authored-by: Andrea Burattin <andrea.burattin@gmail.com>
    Co-authored-by: AFugl <s215207@student.dtu.dk>
    Co-authored-by: Don-Tun <val.neerg@gmail.com>
    Co-authored-by: Andrea Burattin <delas@users.noreply.github.com>
    Co-authored-by: AFugl <fuglsang.anders@gmail.com

---
+++ packages/conferia/frontend/src/router/index.ts
@@ -0,0 +1,133 @@
+import { createRouter, createWebHashHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import TabsPage from '../components/TabsPage.vue'
+import AuthPage from "@/views/auth/AuthPage.vue";
+import ProfilePage from "@/views/auth/ProfilePage.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/tabs/home'
+  },
+
+  {
+    path: '/tabs/',
+    component: TabsPage,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/home'
+      },
+      {
+        path: 'home',
+        component: () => import('@/views/TabHome.vue')
+      },
+      {
+        path: 'calendar/:id?',
+        name: 'agenda',
+        component: () => import('@/views/calendar/TabAgenda.vue'),
+      },
+      {
+        path: '/calendar-view',
+        name: 'CalendarView',
+        component: () => import('@/views/calendar/TabCalendar.vue')
+      },
+      {
+        path: 'attendees',
+        component: () => import('@/views/attendees/TabAttendees.vue'),
+      },
+      {
+        path: '/attendee/:id',
+        name: 'AttendeeDetail',
+        component: () => import('@/views/attendees/TabAttendeeDetail.vue')
+      },
+      {
+        path: 'images/:forceReload?',
+        component: () => import('@/views/gallery/TabGallery.vue')
+      },
+      {
+        path: 'images/myGallery',
+        component: () => import('@/views/gallery/TabMyGallery.vue')
+      },
+      {
+        path: 'singleimage/:imageId',
+        component: () => import('@/views/gallery/TabGallerySingleImage.vue')
+      },
+      {
+        path: 'messages',
+        component: () => import('@/views/messages/TabMessages.vue')
+      },
+      {
+        path: 'page/:id',
+        component: () => import('@/views/pages/TabSinglePage.vue')
+      },
+      {
+        path: 'about',
+        component: () => import('@/views/TabAbout.vue')
+      }
+    ]
+  },
+  {
+    path: '/auth/',
+    component: AuthPage,
+    children: [
+      {
+        path: '',
+        redirect: '/auth/login'
+      },
+      {
+        path: 'login',
+        component: () => import('@/views/auth/AuthLogin.vue')
+      },
+      {
+        path: 'register/:token',
+        component: () => import('@/views/auth/AuthRegister.vue'),
+        props: true // Pass route.params to the component as props
+      },
+      {
+        path: 'login/resetpassword',
+        component: () => import('@/views/auth/AuthResetPassword.vue')
+      },
+      {
+        path: 'login/resetpassword/:token',
+        component: () => import('@/views/auth/AuthResetPasswordWithToken.vue'),
+        props: true // Pass route.params to the component as props
+      }
+    ]
+  },
+  {
+    path: '/profile/',
+    component: ProfilePage,
+    children: [
+      {
+        path:'',
+        redirect: '/profile/settings'
+      },
+      {
+        path:'settings',
+        component: () => import('@/views/auth/ProfileSettings.vue')
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    //Authentication check
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      return next();
+    }
+    return next('/auth/login');
+  }
+  next();
+})
+
+
+export default router
---
+++ packages/conferia/frontend/src/views/calendar/TabCalendar.vue
@@ -0,0 +1,289 @@
+<template>
+  <ion-page>
+    <HeaderBar name="Calendar" />
+
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-button @click="() => {
+          trackButtonClick('Previous Month Calendar', 'Agenda', 'Feature')
+          changeMonth(-1)
+        }">Prev</ion-button>
+      </ion-buttons>
+      <ion-title class="ion-text-center">{{ currentMonthName }} {{ state.currentYear }}</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="() => {
+          trackButtonClick('Next Month Calendar', 'Agenda', 'Feature')
+          changeMonth(1)
+        }">Next</ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+
+
+    <ion-content>
+      <div class="calendar">
+        <div class="day" v-for="day in state.weekDays" :key="day">{{ day }}</div>
+        <div
+            class="date-box"
+            v-for="day in state.daysOfMonth"
+            :key="day.dateString"
+            :class="{ 'not-current': !day.isCurrentMonth }"
+            @click="() => {
+              trackButtonClick('Calendar Date', 'Agenda', 'Feature')
+              day.hasSession ? dateClicked(day): null
+            }"
+        >
+          <div class="date-text">{{ day.date }}</div>
+          <ion-icon :icon="star" v-if="day.hasSession" class="session-dot"></ion-icon>
+        </div>
+      </div>
+    </ion-content>
+
+  </ion-page>
+</template>
+
+
+
+<script setup>
+import { reactive, onMounted, computed, nextTick } from 'vue';
+import {
+  IonPage,
+  IonToolbar,
+  IonContent,
+  IonButton,
+  IonIcon,
+  IonTitle,
+  IonButtons,
+} from '@ionic/vue';
+import HeaderBar from "@/components/HeaderBar.vue";
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+import { star } from 'ionicons/icons';
+import { useRoute } from 'vue-router';
+import backend from "/backend.config.ts";
+import {googleanalytics} from "@/composables/googleanalytics.ts";
+
+const{trackButtonClick} = googleanalytics()
+
+const route = useRoute();
+const router = useRouter();
+const token = localStorage.getItem("accessToken");
+
+const today = new Date();
+const state = reactive({
+  sessions: [],
+  uniqueDays: [],
+  selectedDay: null,
+  daysOfMonth: getDaysOfMonth(today.getFullYear(), today.getMonth()),
+  currentMonth: today.getMonth(),
+  currentYear: today.getFullYear(),
+  // Computed properties for month name and array of week days
+  weekDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+});
+
+const currentMonthName = computed(() => {
+  const date = new Date(state.currentYear, state.currentMonth);
+  return date.toLocaleString('default', { month: 'long' });
+});
+
+function getDaysOfMonth(year, month) {
+  const firstDayOfMonth = new Date(year, month, 1).getDay();
+  const numDays = new Date(year, month + 1, 0).getDate();
+  const days = [];
+
+  // Placeholder days for alignment
+  for (let i = 0; i < firstDayOfMonth; i++) {
+    days.push({
+      date: null,
+      dateString: `placeholder-${i}`,
+      hasSession: false,
+      isCurrentMonth: false
+    });
+  }
+
+  // Actual days of the month
+  for (let day = 1; day <= numDays; day++) {
+    const dateString = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+    days.push({ date: day, dateString, hasSession: false, isCurrentMonth: true });
+  }
+
+  return days;
+}
+
+
+
+function changeMonth(change) {
+  const newDate = new Date(state.currentYear, state.currentMonth + change);
+  state.currentMonth = newDate.getMonth();
+  state.currentYear = newDate.getFullYear();
+
+  // Recalculate days for the new month and update the state
+  state.daysOfMonth = getDaysOfMonth(state.currentYear, state.currentMonth);
+
+  // Assuming sessions do not need to be re-fetched, just re-mark the days
+  markDaysWithSessions(); // Ensure days are re-marked according to the new month
+}
+
+
+
+async function fetchSessions() {
+  try {
+    const { id, type } = route.query;
+    let response;
+
+    if (id) {
+      response = await axios.get(backend.construct(`agenda/session/likedlist/${id}`), {
+        headers: { 'Authorization': `Bearer ${token}` },
+      });
+    } else if (type === 'personal') {
+      const currentUserIdResponse = await axios.get(backend.construct('account/id'), {
+        headers: { 'Authorization': `Bearer ${token}` },
+      });
+      const currentUserId = currentUserIdResponse.data.id;
+      response = await axios.get(backend.construct(`agenda/session/likedlist/${currentUserId}`), {
+        headers: { 'Authorization': `Bearer ${token}` },
+      });
+    } else {
+      response = await axios.get(backend.construct('agenda/sessions'), {
+        headers: { 'Authorization': `Bearer ${token}` },
+      });
+    }
+
+    state.sessions = response.data;
+    markDaysWithSessions();
+  } catch (error) {
+    console.error('Failed to fetch sessions:', error);
+  }
+}
+
+
+
+function markDaysWithSessions() {
+  state.daysOfMonth.forEach(day => {
+    // Check if the day is part of the current month
+    if (!day.isCurrentMonth) return;
+
+    // Look for sessions on this day
+    const hasSession = state.sessions.some(session => {
+      // Adjust to use the correct property name
+      const startTime = session.startTime?.split('T')[0] ?? "";
+      return day.dateString === startTime;
+    });
+
+    // Update the day's hasSession property if applicable
+    if (hasSession) day.hasSession = true;
+  });
+}
+
+
+
+function dateClicked(day) {
+  if (!day.isCurrentMonth || day.date == null) return;
+
+  // Get the clicked date and ensure it's in UTC
+  const clickedDate = new Date(Date.UTC(state.currentYear, state.currentMonth, day.date));
+  const query = { date: clickedDate.toISOString().slice(0, 10) };
+
+  // Extract user ID and agenda type from the route parameters or query
+  const routeUserId = route.params.id || route.query.id;
+  const agendaType = route.query.type;
+
+  // Redirect to the desired path with the ID and date parameter
+  if (routeUserId) {
+    router.push({ path: `/tabs/calendar/${routeUserId}`, query }).then(() => {
+      nextTick(() => window.location.reload());
+    });
+  } else if (agendaType === 'personal') {
+    query.type = 'personal';
+    router.push({ path: '/tabs/calendar', query }).then(() => {
+      nextTick(() => window.location.reload());
+    });
+  } else {
+    router.push({ path: '/tabs/calendar', query }).then(() => {
+      nextTick(() => window.location.reload());
+    });
+  }
+}
+
+
+
+
+
+
+
+const applyTheme = () => {
+  const theme = localStorage.getItem('theme'); // Get the theme from localStorage
+  const rootStyle = document.documentElement.style;
+
+  if (theme === 'dark') {
+    rootStyle.setProperty('--text-color', '#ffffff'); // Dark background color
+    rootStyle.setProperty('--secondary-text-color', '#686868');       // Light text color
+    rootStyle.setProperty('--calendar-background-color', '#2a2a2a')
+    rootStyle.setProperty('--icon-color', '#098BFF')
+
+  } else {
+    rootStyle.setProperty('--calendar-background-color', '#E6E6FA'); // Light background color
+    rootStyle.setProperty('--text-color', '#000000');
+    rootStyle.setProperty('--icon-color', '#000000')// Dark text color
+    // Set other light theme colors as needed
+  }
+};
+
+
+onMounted(() => {
+  fetchSessions();
+  applyTheme();
+});
+
+</script>
+
+
+
+<style>
+
+.calendar {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr); /* 7 columns for 7 days of the week */
+  gap: 1%; /* Space between each "day" box, adjusted to be responsive */
+}
+
+.day, .date-box {
+  aspect-ratio: 1; /* Makes the box square */
+  display: flex;
+  position: relative;
+}
+
+.day {
+  justify-content: center;
+  align-items: center;
+  background-color: transparent; /* No background for day names */
+  color: var(--text-color); /* White color for the day names */
+}
+
+.calendar .date-box {
+  justify-content: flex-end; /* Positions the date in the bottom-right */
+  align-items: flex-end;
+  padding: 5%; /* Responsive padding */
+  cursor: pointer; /* Indicates the item is clickable */
+  background-color: var(--calendar-background-color); /* Slight background color for date boxes */
+  border-radius: 5%; /* Responsive border radius */
+}
+
+.date-text {
+  font-size: 1.2em; /* Slightly larger and responsive font size */
+  color: var(--text-color); /* White color for the numbers */
+}
+
+.session-dot {
+  position: absolute;
+  top: 15%; /* Centers the dot vertically */
+  left: 15%; /* Centers the dot horizontally */
+  transform: translate(-50%, -50%); /* Ensures the dot is perfectly centered */
+  width: 20%; /* Size of the dot in em for responsiveness */
+  height: 20%;
+  color : #098BFF;
+  background-color: var(--calendar-background-color); /* Color of the dot */
+  border-radius: 50%; /* Makes the dot circular */
+}
+
+
+</style>
