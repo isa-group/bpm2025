d1fd1b28a728f9d44522dfc8c6eb3ce06e8a178e
Fernando Fernández <ferferga@hotmail.com>
2025-07-23 11:21:37 +0000

    refactor(conferia-frontend): cleanup deps

--- packages/conferia/frontend/vite.config.ts
+++ packages/conferia/frontend/vite.config.ts
@@ -1,12 +1,10 @@
 import path from 'path';
-import legacy from '@vitejs/plugin-legacy';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
-    vue(),
-    legacy()
+    vue()
   ],
   resolve: {
     alias: {
