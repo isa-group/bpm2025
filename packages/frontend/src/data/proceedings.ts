2033e71753b8d7385898d93aeb814220af7ab0b7
Manuel Resinas <resinas@us.es>
2025-08-28 17:03:12 +0200

    feat(program): proceedings page

---
+++ packages/frontend/src/data/proceedings.ts
@@ -0,0 +1,17 @@
+export const proceedings = {
+  bpm: {
+    name: 'Business Process Management',
+    volume: 'LNCS 16044',
+    href: 'https://link.springer.com/book/9783032028662'
+  },
+  bpmforum: {
+    name: 'Business Process Management Forum',
+    volume: 'LNBIP 564',
+    href: 'https://link.springer.com/book/10.1007/978-3-032-02929-4'
+  },
+  forums: {
+    name: 'Business Process Management: Responsible BPM Forum, Process Technology Forum, Educators Forum',
+    volume: 'LNBIP 565',
+    href: 'https://link.springer.com/book/9783032029355'
+  }
+} as const;
