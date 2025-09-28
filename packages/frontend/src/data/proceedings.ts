0e21bbc7e4b349ec39d5db5396446c4060b49ef6
Manuel Resinas <resinas@us.es>
2025-09-28 13:31:47 +0200

    feat(proceedings):Add CEUR proceedings

--- packages/frontend/src/data/proceedings.ts
+++ packages/frontend/src/data/proceedings.ts
@@ -1,17 +1,37 @@
 export const proceedings = {
   bpm: {
     name: 'Business Process Management',
+    longtitle: 'BPM 2025 main proceedings',
+    shorttitle: 'BPM 2025',
     volume: 'LNCS 16044',
     href: 'https://link.springer.com/book/9783032028662'
   },
   bpmforum: {
     name: 'Business Process Management Forum',
+    longtitle: 'BPM 2025 Forum',
+    shorttitle: 'BPM Forum',
     volume: 'LNBIP 564',
     href: 'https://link.springer.com/book/10.1007/978-3-032-02929-4'
   },
   forums: {
     name: 'Business Process Management: Responsible BPM Forum, Process Technology Forum, Educators Forum',
+    longtitle: 'BPM 2025 Responsible BPM Forum, Process Technology Forum, Educators Forum',
+    shorttitle: 'RBPM, PT, Educators',
     volume: 'LNBIP 565',
     href: 'https://link.springer.com/book/9783032029355'
+  },
+  industry: {
+    name: 'Business Process Management: Industry & Society Forum',
+    longtitle: 'BPM 2025 Industry & Society Forum',
+    shorttitle: 'Industry & Society',
+    volume: 'CEUR-WS, Vol 4033',
+    href: 'https://ceur-ws.org/Vol-4033/'
+  },
+  dcdemos: {
+    name: 'Business Process Management: Best Dissertation Award, Doctoral Consortium, and Demonstration & Resources',
+    longtitle: 'BPM 2025 Best Dissertation Award, Doctoral Consortium, and Demonstration & Resources',
+    shorttitle: 'Best Dissertation, DC, Demos',
+    volume: 'CEUR-WS, Vol 4032',
+    href: 'https://ceur-ws.org/Vol-4032/'
   }
 } as const;
